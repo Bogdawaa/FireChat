1170882de73fe9f6f841ce78c2986484ef4b2459
Bogdawaa <bogdan6246@yandex.ru>
2025-08-15 13:58:25 +0300

    Initial Commit

---
+++ FireChat/FireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatApp.swift
+//  FireChat
+//
+//  Created by Bogdan Fartdinov on 15.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct FireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
